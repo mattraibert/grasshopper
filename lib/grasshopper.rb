dfb3f9b2631d67f6b1d6b7d537373601a3803179
Matt Raibert <mattraibert@gmail.com>
2012-08-31 23:36:55 -0400

    include MiniTest::Assertions so I can use assert instead of silly inlined version; bump minor rev

--- lib/grasshopper.rb
+++ lib/grasshopper.rb
@@ -1,30 +1,23 @@
 require 'minitest/unit'
 
 class Mock
+  include MiniTest::Assertions
+
   def verify_next
     @verify_next = true
   end
 
-  #todo can't figure out how to call assert from here; copying impl
-  def assert test, msg = nil
-    unless test then
-      msg = msg.call if Proc === msg
-      raise MiniTest::Assertion, msg
-    end
-    true
-  end
-
   def method_missing(message, *args, &block)
     @messages ||= []
-    if(@verify_next)
-      index = @messages.index {|item| message == item[0] and item[1] == args}
+    if @verify_next
+      index = @messages.index { |item| message == item[0] and item[1] == args }
       assert(index, "Should have seen an invocation of #{message}")
       @messages.delete_at(index || @messages.length)
     else
       @messages << [message, args]
     end
   end
-  
+
   def self.verify mock
     mock.verify_next
     mock
