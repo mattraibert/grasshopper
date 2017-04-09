583f3686c8fa0324aaff562c9222465be62d09bb
Matt J Raibert <mattraibert@positiondev.com>
2017-04-09 16:05:25 -0400

    Clarify mock internals

--- lib/grasshopper/mock.rb
+++ lib/grasshopper/mock.rb
@@ -11,21 +11,26 @@ module Grasshopper
     def method_missing(message, *args, &block)
       @messages ||= []
       if @verify_next
-        index = @messages.index { |item| message == item[0] and item[1] == args }
+        index = @messages.index { |heard| message == heard.message and heard.args == args }
         unless index
           raise "Should have seen an invocation of #{message}(#{args.join(", ")})\n#{@messages.inspect}"
         end
         @messages.delete_at(index || @messages.length)
       else
-        @messages << [message, args]
+        @messages << MessageHeard.new(message, args)
       end
       nil
     end
+
+    MessageHeard = Struct.new(:message, :args)
 
     def self.verify mock
       raise "Not a #{self.class}" unless mock.is_a? Grasshopper::Mock
       mock.verify_next
       mock
+    end
+    class AnyParam
+
     end
   end
 end
